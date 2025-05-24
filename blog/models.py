b423039f049f603617f073d5c679a133dd53ea3b
omogbolahan94 <gabrielomogbolahan1@gmail.com>
2025-05-24 14:06:11 +0100

    post form created

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
 
@@ -13,3 +14,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self): 
+        """
+        This function tells Django that the ultimate location of a Post entry is 
+        its post_detail view which is posts/<int:pk>
+
+        So that every time a new post is created, the function will be called 
+
+        Reverse is a very handy utility function Django provides us to reference an object by its URL
+        template name, in this case post_detail.
+        In this case, the template for post_details requires a primary key argument: path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+        """
+        return reverse('post_detail', args=[str(self.id)])
