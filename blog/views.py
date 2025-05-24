439d5cebfe03bc5116a04df7a0aa7c40e2883b05
omogbolahan94 <gabrielomogbolahan1@gmail.com>
2025-05-24 12:47:16 +0100

    added detail view for each post and test script

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class BlogListView(ListView):
+    """
+    ListView will give us access to object_list whuchcontains all the objects in this view
+    It will be used in the home.html to iterate through all blog post of the Post model
+    """
+    model = Post # model for the view
+    template_name = 'home.html' # html we want to render
+
+
+class BlogDetailView(DetailView): 
+    """
+    By default, DetailView will provide a context object we can use in our
+    template called either 'object' or the lowercased name of our model, which would be 'post'.
+
+    This context obhect helps us to access the data in our model
+    """
+    model = Post
+    template_name = 'post_detail.html'
