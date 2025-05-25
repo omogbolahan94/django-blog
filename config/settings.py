539f55778e4be150c512288f757cd6cc7c9a4578
omogbolahan94 <gabrielomogbolahan1@gmail.com>
2025-05-25 06:07:44 +0100

    login and logout page

--- config/settings.py
+++ config/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
+# redirect to home page on a successful login or logout
+LOGIN_REDIRECT_URL = 'home' 
+LOGOUT_REDIRECT_URL = 'home' 
